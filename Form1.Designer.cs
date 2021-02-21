3ba255f1236b67172f5f89899fb449b456eba5c4
Sothoth <Sothoth@PANTHEON>
2021-02-21 18:06:40 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace integral
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReady = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtot = new System.Windows.Forms.TextBox();
+            this.txtdo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(190, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "F(x) = x ^ 2";
+            // 
+            // btnReady
+            // 
+            this.btnReady.Enabled = false;
+            this.btnReady.Location = new System.Drawing.Point(60, 238);
+            this.btnReady.Name = "btnReady";
+            this.btnReady.Size = new System.Drawing.Size(110, 34);
+            this.btnReady.TabIndex = 1;
+            this.btnReady.Text = "Найти";
+            this.btnReady.UseVisualStyleBackColor = true;
+            this.btnReady.Click += new System.EventHandler(this.btnReady_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(9, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "от";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(102, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "до";
+            // 
+            // txtot
+            // 
+            this.txtot.Location = new System.Drawing.Point(38, 122);
+            this.txtot.Name = "txtot";
+            this.txtot.Size = new System.Drawing.Size(58, 20);
+            this.txtot.TabIndex = 4;
+            this.txtot.TextChanged += new System.EventHandler(this.txtot_TextChanged);
+            // 
+            // txtdo
+            // 
+            this.txtdo.Location = new System.Drawing.Point(132, 122);
+            this.txtdo.Name = "txtdo";
+            this.txtdo.Size = new System.Drawing.Size(58, 20);
+            this.txtdo.TabIndex = 5;
+            this.txtdo.TextChanged += new System.EventHandler(this.txtdo_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "График:";
+            // 
+            // txtOut
+            // 
+            this.txtOut.AcceptsReturn = true;
+            this.txtOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtOut.Location = new System.Drawing.Point(248, 87);
+            this.txtOut.Multiline = true;
+            this.txtOut.Name = "txtOut";
+            this.txtOut.ReadOnly = true;
+            this.txtOut.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOut.Size = new System.Drawing.Size(295, 185);
+            this.txtOut.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(323, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(164, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "dx = (x ^ 3) / 3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 181);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(58, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(14, 162);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(202, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Точек после запятой в отвте:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 287);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtdo);
+            this.Controls.Add(this.txtot);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnReady);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnReady;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtot;
+        private System.Windows.Forms.TextBox txtdo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtOut;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace integral
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        double dotA, dotB, y;
+        int precision;
+
+        private void EmptyString()
+        {
+            if (txtot.TextLength == 0)
+                txtot.BackColor = Color.Snow;
+
+            if (txtdo.TextLength == 0)
+                txtdo.BackColor = Color.Snow;
+            if (textBox1.TextLength == 0)
+                textBox1.BackColor = Color.Snow;
+        }
+
+        private void txtdo_TextChanged(object sender, EventArgs e)
+        {
+            EmptyString();
+
+            if (txtdo.TextLength > 0)
+            {
+                try
+                {
+                    dotB = Convert.ToDouble(txtdo.Text);
+                    txtdo.BackColor = Color.White;
+                }
+                catch
+                {
+                    txtdo.BackColor = Color.IndianRed;
+                }
+
+                if (txtot.BackColor == Color.White && txtdo.BackColor == Color.White && textBox1.BackColor == Color.White)
+                    btnReady.Enabled = true;
+                else
+                    btnReady.Enabled = false;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            EmptyString();
+
+            if (textBox1.TextLength > 0)
+            {
+                try
+                {
+                    precision = Convert.ToInt32(textBox1.Text);
+                    textBox1.BackColor = Color.White;
+                }
+                catch
+                {
+                    textBox1.BackColor = Color.IndianRed;
+                }
+
+                if (txtot.BackColor == Color.White && txtdo.BackColor == Color.White && textBox1.BackColor == Color.White)
+                    btnReady.Enabled = true;
+                else
+                    btnReady.Enabled = false;
+            }
+        }
+
+        static double myRound(double x, int precision)
+        {
+            return ((int)(x * Math.Pow(10.0, precision)) / Math.Pow(10.0, precision));
+        }
+
+        private void txtot_TextChanged(object sender, EventArgs e)
+        {
+            EmptyString();
+
+            if (txtot.TextLength > 0)
+            {
+                try
+                {
+                    dotA = Convert.ToDouble(txtot.Text);
+                    txtot.BackColor = Color.White;
+                }
+                catch
+                {
+                    txtot.BackColor = Color.IndianRed;
+                }
+
+                if (txtot.BackColor == Color.White && txtdo.BackColor == Color.White && textBox1.BackColor == Color.White)
+                    btnReady.Enabled = true;
+                else
+                    btnReady.Enabled = false;
+            }
+
+        }
+
+        private void btnReady_Click(object sender, EventArgs e)
+        {            
+            txtOut.Text = string.Empty;
+
+            for (double i = dotA; i <= dotB; i++)
+            {
+                y = (Math.Pow(i, 3)) / 3;
+                y = myRound(y, precision);
+                txtOut.Text += $"при x = {i}, y = {y}" + Environment.NewLine;
+            }
+
+        }
+    }
+}
